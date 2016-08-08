87907cc3273c7876880325f7d6bc5ec4533c07d1
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-08-08 11:15:49 +0200

    wow

--- script.php
+++ script.php
@@ -6,9 +6,32 @@ if ($dir = opendir($path))
 {
   while (($file = readdir($dir)) != false)
   {
-    echo "Fichier :".$file."\n";
+    if ($file != "." && $file != "..") {
+      handle_file($path."/".$file."/cttime", $file);
+    }
   }
   closedir($dir);
+}
+
+function handle_file($path, $id) {
+  $handle = fopen($path, "r");
+  $line = fgets($handle, EOF);
+  if ($line == "d") {
+    return;
+  } else if ($line == "e") {
+    exec("vzctl stop ".$id);
+  } else if ($line == "x") {
+    exec("vzctl destroy ".$id);
+  } else {
+    $cttime = intval(strval($line));
+    $curtime = time();
+    exec("vzctl status ".$id, $output);
+    $state = $output[0];
+    echo "vm ".$id;
+    echo "cttime = ".$cttime;
+    echo $state;
+  }
+  fclose($handle);
 }
 
 ?>
