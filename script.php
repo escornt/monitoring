13375dce45d9ce955f32f33f165e1b5d7cbc52f0
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-08-08 14:20:26 +0200

    wow

--- script.php
+++ script.php
@@ -14,21 +14,28 @@ if ($dir = opendir($path))
 }
 
 function handle_file($path, $id) {
+  $EXT_TIME = 1296000;
+  $DEST_TIME = $EXT_TIME * 2;
+  // test si le fichier existe
   if (file_exists($path)) {
     $handle = fopen($path, "r+");
     $line = fgets($handle, 255);
+    // d => disabled, ignore la ct
     if (strcmp($line, "d\n") == 0) {
       fclose($handle);
       return;
+    // e => sera eteinte au porchain passage
     } else if (strcmp($line, "e\n") == 0) {
       fclose($handle);
       exec("vzctl stop ".$id);
       exec("date +%s > ".$path);
       return;
+    // x => sera detruite au prochain passage
     } else if (strcmp($line, "x\n") == 0) {
       fclose($handle);
       exec("vzctl destroy ".$id);
       return;
+    // Si pas de commande, comparaisons des timestamps
     } else {
       $cttime = intval(strval($line));
       $curtime = time();
@@ -37,21 +44,20 @@ function handle_file($path, $id) {
       $state = substr(substr($state,strrpos(trim($state),' ')), 1);
       $dif = $curtime - $cttime;
       if (strcmp($state, "running") == 0) {
-        echo "RUNNING CONFIRMED";
-        if ($dif > 1296000) {
+        if ($dif > $EXT_TIME) {
           exec("echo e > ".$path);
         }
       } else if (strcmp($state, "down") == 0) {
-        echo "DOWN CONFIRMED";
-        if ($dif > 1296000 * 2) {
+        if ($dif > $DEST_TIME) {
           exec("echo e > ".$path);
         }
       }
+      /* AFFICHAGE DES VARIABLES
       echo "Contenue du fichier = ".$line."\n";
       echo "Ct ".$id."\n";
       echo "Ct time = ".$cttime."\n";
       echo "Current time = ".$curtime."\n";
-      echo "State = ".$state."\n";
+      echo "State = ".$state."\n";*/
     }
     fclose($handle);
   }
